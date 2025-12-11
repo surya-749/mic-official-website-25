4f82b9a5f90a7e4e8c06d1a4f30f2bb4a39fcea3
Surya R <ravindransurya2008@gmail.com>
2025-12-11 13:36:58 +0530

    refactor(model-scene): remove rotation logic and simplify component structure

--- src/app/main/_components/model-scene.tsx
+++ src/app/main/_components/model-scene.tsx
@@ -3,56 +3,44 @@
 import React, { Suspense, useRef } from "react";
 import * as THREE from "three";
 import Model from "./models/model";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import {
   Environment,
   OrbitControls,
 } from "@react-three/drei";
 
-const RotatingModel: React.FC = () => {
-  const ref = useRef<THREE.Group>(null);
-
-  const speed = useRef({
-    x: (Math.random() - 0.5) * 0.02,
-    y: (Math.random() - 0.5) * 0.02,
-    z: (Math.random() - 0.5) * 0.02,
-  });
-
-  useFrame(() => {
-    if (ref.current) {
-      ref.current.rotation.x += speed.current.x;
-      ref.current.rotation.y += speed.current.y;
-      ref.current.rotation.z += speed.current.z;
-    }
-  });
-
-  return (
-    <Model
-      rotation={[
-        Math.PI / 2,
-        3 * (Math.PI / 2) * 1.1,
-        (Math.PI / 4) * 1.1,
-      ]}
-      ref={ref}
-    />
-  );
-};
-
 const ModelScene: React.FC = () => {
   return (
-    <Canvas
+    <div
       style={{
-        width: "100%",
-        height: "80vh",
+        position: 'relative',
+        width: '90vw',
+        maxWidth: 600,
+        height: 420,
+        margin: '3rem auto 0',
+        zIndex: 11,
       }}
-      camera={{ position: [0, 0, 8], fov: 50 }}
     >
-      <OrbitControls />
-      <Suspense fallback={null}>
-        <RotatingModel />
-        <Environment preset="sunset" />
-      </Suspense>
-    </Canvas>
+      <Canvas
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        camera={{ position: [0, 0, 8], fov: 50 }}
+      >
+        <OrbitControls enablePan={false} enableZoom={false} enableDamping dampingFactor={0.05} />
+        <Suspense fallback={null}>
+          <Model
+            rotation={[
+              Math.PI / 2,
+              3 * (Math.PI / 2) * 1.1,
+              (Math.PI / 4) * 1.1,
+            ]}
+          />
+          <Environment preset="sunset" />
+        </Suspense>
+      </Canvas>
+    </div>
   );
 };
 
